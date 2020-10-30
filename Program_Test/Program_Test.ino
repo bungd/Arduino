1de480b7ce42ddab5f902a89f192ee8c9296b776
Bung Daka Putera <bungdaka217@gmail.com>
2020-10-30 07:36:24 +0700

    Create Program_Test.ino

---
+++ Program_Test/Program_Test.ino
@@ -0,0 +1,63 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+
+SoftwareSerial mySerial(7, 6); // RX, TX
+
+int waterlv, soil;
+int minWaterlv = 0;
+int maxWaterlv = 255;
+int minSoil = 0;
+int maxSoil = 255;
+
+String id_user = "1";
+String id_product = "1";
+
+unsigned long lastMillis = 0;
+unsigned long lastMillis1 = 0;
+const long interval = 10000; // interval at which to blink (milliseconds)
+
+void setup() {
+  Serial.begin(115200);
+  mySerial.begin(115200);
+  while (!Serial);
+
+  Wire.begin();
+}
+
+void loop() {
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+
+  readsensor();
+  String nilai = "";
+  nilai += id_user;
+  nilai += ",";
+  nilai += id_product;
+  nilai += ",";
+  nilai += String(waterlv);
+  nilai += ",";
+  nilai += String(soil);
+  nilai += ",";
+
+  if (millis() - lastMillis > interval)
+  {
+    lastMillis = millis();
+    mySerial.println(nilai);
+    Serial.println(nilai);
+  }
+
+}
+
+void readsensor() {
+  static unsigned long timepoint = millis();
+  if (millis() - timepoint > 1000U) //time interval: 1s
+  {
+    timepoint = millis();
+    int value_waterlv = analogRead(A0);
+    int value_soil = analogRead(A1);
+    waterlv = map(value_waterlv, minWaterlv, maxWaterlv, 0, 100);
+
+    soil = map(value_soil, minSoil, maxSoil, 0, 100);
+  }
+}
